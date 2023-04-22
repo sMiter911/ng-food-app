5217e2439bef89b32d1b383c25edca5e77393739
Wandu <31390092+sMiter911@users.noreply.github.com>
2023-04-22 08:34:32 +0200

    WIP: The basics, preserveWhitespaces set to true

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZcHjFzVCSAR7q-4iQSTahMzwEyYRTIm9fzg&usqp=CAU'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZcHjFzVCSAR7q-4iQSTahMzwEyYRTIm9fzg&usqp=CAU'
+    ),
+  ];
 }
