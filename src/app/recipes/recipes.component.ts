85d997c7be7da84ee755bf9213fc57ec83a96a41
Wandu <31390092+sMiter911@users.noreply.github.com>
2023-06-22 20:04:59 +0200

    WIP: Components and Data Binding

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent {
-
+  selectedRecipe!: Recipe;
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+  styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: true }) nameInputRef!: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  addRecipe() {
+    const newIngName = this.nameInputRef.nativeElement.value;
+    const newIngAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(newIngName, newIngAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
